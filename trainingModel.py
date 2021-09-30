b6611f614534098cca63d1aeb484a40a7777e1f9
aman-sharma7m <aman.7m@outlook.com>
2021-09-30 12:55:09 +0530

    model train lib setup done

---
+++ trainingModel.py
@@ -0,0 +1,14 @@
+"""
+This is the main work for training the model
+prepared by:Aman
+"""
+from sklearn.model_selection import train_test_split
+from data_ingestion import data_loader
+from data_preprocessing import preprocessing
+from data_preprocessing import clustering
+from best_model_finder import tuner
+from application_logging import logger
+from file_operations import file_methods
+
+class trainModel:
+    pass
