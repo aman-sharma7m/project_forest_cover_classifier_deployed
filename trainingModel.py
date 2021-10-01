19475b778cdc4093252dcda5be5aebaac228d471
aman-sharma7m <aman.7m@outlook.com>
2021-10-02 01:50:04 +0530

    done with file methodlogy

--- trainingModel.py
+++ trainingModel.py
@@ -48,6 +48,31 @@ class trainModel:
             list_of_clusters = X['Cluster'].unique()
 
             """parsing all the clusters and looking for the best ML algorithm to fit on individual cluster"""
+            for i in list_of_clusters:
+                cluster_data = X[X['Cluster'] == i]  # filter the data for one cluster
+
+                # Prepare the feature and Label columns
+                cluster_features = cluster_data.drop(['Labels', 'Cluster'], axis=1)
+                cluster_label = cluster_data['Labels']
+
+                # splitting the data into training and test set for each cluster one by one
+                x_train, x_test, y_train, y_test = train_test_split(cluster_features, cluster_label, test_size=1 / 3,
+                                                                    random_state=355)
+                x_train = preprocessor.scaleData(x_train)
+                x_test = preprocessor.scaleData(x_test)
+
+                model_finder = tuner.Model_Finder(self.file_object, self.log_writer)  # object initialization
+
+                # getting the best model for each of the clusters
+                best_model_name, best_model = model_finder.get_best_model(x_train, y_train, x_test, y_test)
+
+                # saving the best model to the directory.
+                file_op = file_methods.File_Operation(self.file_object, self.log_writer)
+                save_model = file_op.save_model(best_model, best_model_name + str(i))
+
+                # logging the successful Training
+            self.log_writer.log(self.file_object, 'Successful End of Training')
+            self.file_object.close()
 
 
         except Exception:
