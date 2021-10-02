743ae1d6a2b71f92a2f57e2946acbecf7b16e125
aman-sharma7m <aman.7m@outlook.com>
2021-10-02 22:53:40 +0530

    prediction_Validation_Insertion start

--- prediction_Validation_Insertion.py
+++ prediction_Validation_Insertion.py
@@ -3,3 +3,13 @@ from application_logging import logger
 from Prediction_Raw_Data_Validation.predictionDataValidation import Prediction_Data_validation
 from DataTypeValidation_Insertion_Prediction.DataTypeValidationPrediction import dBOperation
 from DataTransformation_Prediction.DataTransformationPrediction import dataTransformPredict
+
+
+class pred_Validation:
+    def __init__(self,path):
+        self.raw_data=Prediction_Data_validation(path)
+        self.dataTransformation=dataTransformPredict()
+        self.dBOperation=dBOperation()
+        self.file_object=open('Prediction_Logs/Prediction_Log.txt','a+')
+        self.log_writer=logger.App_Logger()
+
